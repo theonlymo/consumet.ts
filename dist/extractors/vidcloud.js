972d4cbbd0c1d0907e0f49a0d0565baa234bfc01
Mohamed Fouad <>
2024-10-09 11:00:32 +0200

    update rabbit

--- dist/extractors/vidcloud.js
+++ dist/extractors/vidcloud.js
@@ -1,63 +1,40 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const rabbit_1 = require("./rabbit");
 const models_1 = require("../models");
-const utils_1 = require("../utils");
 class VidCloud extends models_1.VideoExtractor {
     constructor() {
         super(...arguments);
         this.serverName = 'VidCloud';
         this.sources = [];
-        this.extract = async (videoUrl, _) => {
-            var _a;
+        this.extract = async (videoUrl, isVidcloud = false) => {
             const result = {
                 sources: [],
                 subtitles: [],
             };
             try {
-                const id = (_a = videoUrl.href.split('/').pop()) === null || _a === void 0 ? void 0 : _a.split('?')[0];
-                const options = {
-                    headers: {
-                        'X-Requested-With': 'XMLHttpRequest',
-                        Referer: videoUrl.href,
-                        'User-Agent': utils_1.USER_AGENT,
-                    },
-                };
-                const res = await (0, rabbit_1.main)(id);
-                const sources = res.sources;
-                this.sources = sources.map((s) => ({
-                    url: s.file,
-                    isM3U8: s.file.includes('.m3u8') || s.file.endsWith('m3u8'),
-                }));
-                result.sources.push(...this.sources);
-                result.sources = [];
-                this.sources = [];
-                for (const source of sources) {
-                    const { data } = await this.client.get(source.file, options);
-                    const urls = data
-                        .split('\n')
-                        .filter((line) => line.includes('.m3u8') || line.endsWith('m3u8'));
-                    const qualities = data.split('\n').filter((line) => line.includes('RESOLUTION='));
-                    const TdArray = qualities.map((s, i) => {
-                        const f1 = s.split('x')[1];
-                        const f2 = urls[i];
-                        return [f1, f2];
+                const rabbit_url = process.env.RABBIT_URL;
+                let res = await this.client.post(`${rabbit_url}/${isVidcloud ? "vidcloud" : "upcloud"}`, { "url": videoUrl.href });
+                const { data } = await this.client.get(res.data.source);
+                const urls = data.split('\n').filter((line) => line.includes('.m3u8'));
+                const qualities = data.split('\n').filter((line) => line.includes('RESOLUTION='));
+                const TdArray = qualities.map((s, i) => {
+                    const f1 = s.split('x')[1];
+                    const f2 = urls[i];
+                    return [f1, f2];
+                });
+                for (const [f1, f2] of TdArray) {
+                    result.sources.push({
+                        url: f2,
+                        quality: f1,
+                        isM3U8: f2.includes('.m3u8'),
                     });
-                    for (const [f1, f2] of TdArray) {
-                        this.sources.push({
-                            url: f2,
-                            quality: f1,
-                            isM3U8: f2.includes('.m3u8') || f2.endsWith('m3u8'),
-                        });
-                    }
-                    result.sources.push(...this.sources);
                 }
                 result.sources.push({
-                    url: sources[0].file,
-                    isM3U8: sources[0].file.includes('.m3u8') || sources[0].file.endsWith('m3u8'),
+                    url: res.data.source,
+                    isM3U8: res.data.source.includes('.m3u8'),
                     quality: 'auto',
                 });
-                result.subtitles = res.tracks.map((s) => ({
+                result.subtitles = res.data.subtitle.map((s) => ({
                     url: s.file,
                     lang: s.label ? s.label : 'Default (maybe)',
                 }));
